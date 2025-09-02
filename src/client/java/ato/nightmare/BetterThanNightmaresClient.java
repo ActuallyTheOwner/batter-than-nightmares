60ab8f1c707e7f0308150fd0de63c0ae1d4adc15
ActuallyTheOwner <wellswolelouie@yahoo.com>
2025-09-02 16:08:04 -0500

    Initial commit

---
+++ src/client/java/ato/nightmare/BetterThanNightmaresClient.java
@@ -0,0 +1,10 @@
+package ato.nightmare;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterThanNightmaresClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/ato/nightmare/BetterThanNightmaresDataGenerator.java
@@ -0,0 +1,11 @@
+package ato.nightmare;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterThanNightmaresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
